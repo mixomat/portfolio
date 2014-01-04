51c4e0ea2c84ccdac0bdaebaf00a4d2db93b727b
Marc Weinberger <marc.weinberger@me.com>
2014-01-05 00:02:45 +0100

    added projects controller part to main.html

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,6 @@
 'use strict';
 
 angular.module('portfolioApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', function () {
+
+});
