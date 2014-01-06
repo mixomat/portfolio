77151c4dc0edf7e91c1a8bc90d95a9457f5b01de
Marc Weinberger <marc.weinberger@me.com>
2014-01-06 11:56:19 +0100

    code cleanup

--- test/spec/controllers/project.js
+++ test/spec/controllers/project.js
@@ -1,33 +1,33 @@
-'use strict'
+'use strict';
 
 describe('Controller: ProjectCtrl', function () {
   // load the app module
   beforeEach(module('portfolioApp'));
 
   describe('ProjectCtrl', function () {
-    var createController, scope, httpBackend;
+    var createController, $scope, $httpBackend;
 
-    beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
-      // register the httpBackend mock
-      httpBackend = $httpBackend;
-      httpBackend.when('GET', 'http://localhost:3000/api/projects').respond([
+    beforeEach(inject(function ($controller, _$rootScope_, _$httpBackend_) {
+      $httpBackend = _$httpBackend_;
+      $scope = _$rootScope_.$new();
+
+      // register the $httpBackend mock
+      $httpBackend.when('GET', 'http://localhost:3000/api/projects').respond([
         {projects: {title: 'test'}}
       ]);
 
-      // Initialize the controller and a mock scope
-      scope = $rootScope.$new();
+      // Initialize the controller and a mock $scope
       createController = function () {
-        return $controller('ProjectCtrl', {$scope: scope});
+        return $controller('ProjectCtrl', {$scope: $scope});
       };
-
     }));
 
-    it('should attach projects to the scope', function () {
-      httpBackend.expectGET('http://localhost:3000/api/projects');
+    it('should attach projects to the $scope', function () {
+      $httpBackend.expectGET('http://localhost:3000/api/projects');
       createController();
-      httpBackend.flush();
+      $httpBackend.flush();
 
-      expect(scope.projects.length).toBe(1);
+      expect($scope.projects.length).toBe(1);
     });
   });
 });
