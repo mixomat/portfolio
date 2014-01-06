57f3b5cfae9a8852f1d4265e034a0448aae6f458
Marc Weinberger <marc.weinberger@me.com>
2014-01-06 18:16:43 +0100

    added simple paging of skills

---
+++ test/spec/filters/startfrom.js
@@ -0,0 +1,25 @@
+'use strict';
+
+describe('Filter: startFrom', function () {
+  beforeEach(module('portfolioApp'));
+
+  // initialize a new instance of the filter before each test
+  var startFrom;
+  beforeEach(inject(function ($filter) {
+    startFrom = $filter('startFrom');
+  }));
+
+  it('should start from the beginning 0', function () {
+    expect(startFrom([1,2,3,4],0)).toEqual([1,2,3,4]);
+  });
+
+  it('should start from index 1', function () {
+    expect(startFrom([1,2,3,4],1)).toEqual([2,3,4]);
+  });
+
+  it('should be empty', function () {
+    expect(startFrom([1,2,3,4],4)).toEqual([]);
+  });
+
+
+});
