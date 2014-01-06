01b471cfa28e4b0be16a066c6dec8c84aaa8f54d
Marc Weinberger <marc.weinberger@me.com>
2014-01-06 11:59:45 +0100

    test cleanup

--- test/spec/controllers/project.js
+++ test/spec/controllers/project.js
@@ -5,25 +5,25 @@ describe('Controller: ProjectCtrl', function () {
   beforeEach(module('portfolioApp'));
 
   describe('ProjectCtrl', function () {
+
     var createController, $scope, $httpBackend;
 
     beforeEach(inject(function ($controller, _$rootScope_, _$httpBackend_) {
       $httpBackend = _$httpBackend_;
       $scope = _$rootScope_.$new();
 
-      // register the $httpBackend mock
+      // register the $httpBackend mock calls
       $httpBackend.when('GET', 'http://localhost:3000/api/projects').respond([
         {projects: {title: 'test'}}
       ]);
+      $httpBackend.expectGET('http://localhost:3000/api/projects');
 
       // Initialize the controller and a mock $scope
       createController = function () {
         return $controller('ProjectCtrl', {$scope: $scope});
       };
     }));
-
     it('should attach projects to the $scope', function () {
-      $httpBackend.expectGET('http://localhost:3000/api/projects');
       createController();
       $httpBackend.flush();
 
