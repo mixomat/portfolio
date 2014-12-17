380c26957df37343a69bb740764b38c09cd97d77
Marc Weinberger <marc.weinberger@me.com>
2014-12-17 23:20:23 +0100

    removed resume section, improved skills section

--- app/scripts/controllers/skillCtrl.js
+++ app/scripts/controllers/skillCtrl.js
@@ -2,15 +2,15 @@
 
 angular.module('portfolioApp').controller('SkillCtrl', function ($scope) {
   $scope.skills = [
-    {id: 'java', skill: 'Java/JEE', knowledge: 90},
-    {id: 'spring', skill: 'Spring', knowledge: 97},
-    {id: 'ruby', skill: 'Ruby', knowledge: 55},
-    {id: 'javascript', skill: 'Javascript', knowledge: 80},
-    {id: 'htmlcss', skill: 'HTML/CSS', knowledge: 90},
+    {id: 'java', skill: 'Java/JEE', knowledge: 95},
+    {id: 'spring', skill: 'Spring', knowledge: 99},
     {id: 'ios', skill: 'iOS', knowledge: 80},
     {id: 'android', skill: 'Android', knowledge: 65},
-    {id: 'hadoop', skill: 'Hadoop', knowledge: 40}
+    {id: 'ruby', skill: 'Ruby', knowledge: 65},
+    {id: 'hadoop', skill: 'Hadoop', knowledge: 53},
+    {id: 'javascript', skill: 'Javascript', knowledge: 80},
+    {id: 'angular', skill: 'AngularJS', knowledge: 75}
   ];
-  $scope.pageSize = $scope.skills.length;
-  $scope.pages = [0,1];
+  $scope.pageSize = 4;
+  $scope.pages = [0,1,2];
 });
