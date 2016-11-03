3469f9f9e529a1b0b6d43bbf977a586fc7c49f5f
Marc Weinberger <marc.weinberger@me.com>
2016-11-03 20:56:51 +0100

    test(improve projects tests):

--- src/app/projects.test.js
+++ src/app/projects.test.js
@@ -3,7 +3,36 @@ import {request} from './projects';
 
 describe('projects', () => {
 
-  it('should define a request function', () => {
-    expect(request).to.be.a('function');
+  describe('request()', () => {
+    it('should be a function', () => {
+      expect(request).to.be.a('function');
+    });
+
+    it('should emit a request object', (done) => {
+      request().subscribe({
+        next: (request) => {
+          expect(request).to.be.a('Object');
+        },
+        complete: done
+      });
+    });
+
+    it('should emit a request object with a valid url', (done) => {
+      request().subscribe({
+        next: (request) => {
+          expect(request.url).to.be.equal('/api/projects');
+        },
+        complete: done
+      });
+    });
+
+    it('should emit a request object with a valid category', (done) => {
+      request().subscribe({
+        next: (request) => {
+          expect(request.category).to.be.equal('projects');
+        },
+        complete: done
+      });
+    })
   });
 });
